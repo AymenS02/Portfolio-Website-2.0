d295997d2fdd209ce2b2bfb805e2a90509f9d48e
Aymen Shoteri <aymenshoteri@gmail.com>
2024-03-24 09:04:01 -0400

    redone skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -13,16 +13,7 @@ const Skills = () => {
             <img src="public/c++.webp" alt="c++" />
             <img src="public/Haskell-Logo.svg" alt="html" />
         </div>
-        <div className="skills-slides">
-            <img src="public/python.webp" alt="python" />
-            <img src="public/javascript.svg" alt="javascript" />
-            <img src="public/react.webp" alt="react" />
-            <img src="public/node.webp" alt="node" />
-            <img src="public/postgresql.svg" alt="postgresql" />
-            <img src="public/java.webp" alt="java" />
-            <img src="public/c++.webp" alt="c++" />
-            <img src="public/Haskell-Logo.svg" alt="html" />
-        </div>
+
     </div>
   )
 }
