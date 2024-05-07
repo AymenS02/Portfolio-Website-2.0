1be57735a96c2fdca5c95d070d86a9af85800609
Aymen Shoteri <aymenshoteri@gmail.com>
2024-05-07 14:36:40 -0400

    editing skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -25,21 +25,36 @@ const Skills = () => {
   return (
     <div className="skills">
       <motion.ul
-        className="skills"
+        className="skills-ul"
         variants={container}
         initial="hidden"
         animate="visible"
       >
-        <motion.li className="skills-slides" variants={item}>
+        <motion.li className="skills-li" variants={item}>
           <img src="public/python.webp" alt="python" />
-          <img src="public/javascript.svg" alt="javascript" />
+        </motion.li>
+        <motion.li className="skills-li" variants={item}>
+        <img src="public/javascript.svg" alt="javascript" />
+        </motion.li>
+        <motion.li className="skills-li" variants={item}>
           <img src="public/react.webp" alt="react" />
+        </motion.li>
+        <motion.li className="skills-li" variants={item}>
           <img src="public/node.webp" alt="node" />
+        </motion.li>
+        <motion.li className="skills-li" variants={item}>
           <img src="public/postgresql.svg" alt="postgresql" />
+        </motion.li>
+        <motion.li className="skills-li" variants={item}>
           <img src="public/java.webp" alt="java" />
+        </motion.li>
+        <motion.li className="skills-li" variants={item}>
           <img src="public/c++.webp" alt="c++" />
+        </motion.li>
+        <motion.li className="skills-li" variants={item}>
           <img src="public/Haskell-Logo.svg" alt="html" />
         </motion.li>
+
       </motion.ul>
     </div>
   )
