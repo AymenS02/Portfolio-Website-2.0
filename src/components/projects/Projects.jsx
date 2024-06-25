011b0d5943f3be5789460db23c6398d17d9759de
Aymen Shoteri <aymenshoteri@gmail.com>
2024-06-25 16:26:28 -0400

    working on projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -15,18 +15,24 @@ const Projects = () => {
             <div className="project">
                 <h2>Project 1</h2>
                 <p>Project 1 description</p>
+                <img src="public\projects\cointracker.png" alt="coin project image" />
             </div>
             <div className="project">
                 <h2>Project 2</h2>
                 <p>Project 2 description</p>
+                <img src="public\projects\personalPort.png" alt="coin project image" />
+
             </div>
             <div className="project">
                 <h2>Project 3</h2>
                 <p>Project 3 description</p>
+                <img src="public\projects\dynamic_sun.mp4" alt="coin project image" />
+
             </div>
             <div className="project">
                 <h2>Project 4</h2>
                 <p>Project 4 description</p>
+                <img src="public\projects\connect4.png" alt="coin project image" />
             </div>
         </div>
         </motion.div>
