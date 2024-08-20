a754dbd22fb18d0795c712bea6b3be3c86bf9992
Aymen Shoteri <aymenshoteri@gmail.com>
2024-08-20 07:36:42 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,12 @@ function App() {
       <section id="Experience" className='sectionExperience'>
         <Experience />
       </section>
-      {/* <section id="Skills" className='sectionSkills'>
+      <section id="Skills" className='sectionSkills'>
         <Skills />
       </section>
       <section id="Projects" className='sectionProjects'>
         <Projects />
-      </section> */}
+      </section> 
       <footer className='footer'>
         <p>© 2024 by <a href="https://www.linkedin.com/in/aymen-shoteri/" target="_blank" rel="noreferrer">Aymen Shoteri</a></p>
         <p>All Rights Reserved, <a href="https://github.com/AymenS02/Portfolio-Website-2.0" target='_blank' rel="noreferrer" >Website Designed + Developed by Aymen Shoteri</a></p>
